1cf6255da3c19812d583933c743418a02e0277ee
unknown <you@example.com>
2022-01-03 11:43:47 +0600

    My Git Commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoicmFpaGFuOTgiLCJhIjoiY2t3Y3p4bnl5Z3lscjJwcGc3YzU3YnF0YyJ9.dp3kvaq-Y55oV11Zhe9vBg';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11'
+});
